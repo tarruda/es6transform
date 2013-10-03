3b7dbd2109bd98d91be3fade6767bf9508268518
Thiago de Arruda <tpadilha84@gmail.com>
2013-10-03 17:18:49 -0300

    Only bind 'this' when arrow functions have
    'ThisExpression' nodes

--- src/arrow_functions.js
+++ src/arrow_functions.js
@@ -1,3 +1,29 @@
+var estraverse = require('estraverse');
+
+
+function thisMaterialized(arrowFunc) {
+  var rv = false;
+
+  estraverse.traverse(arrowFunc, {
+    enter: function(node) {
+      if (node.type === 'FunctionExpression' ||
+          node.type === 'FunctionDeclaration') {
+        // normal function introduces a new dynamic 'this' scope, no need
+        // to descend
+        return estraverse.VisitorOption.SKIP;
+      } 
+
+      if (node.type === 'ThisExpression') {
+        rv = true;
+        return estraverse.VisitorOption.BREAK;
+      }
+    }
+  });
+
+  return rv;
+}
+
+
 exports.enter = function(node) {
   var rv = {
     type: 'FunctionExpression',
@@ -10,16 +36,19 @@ exports.enter = function(node) {
     expression: false
   };
 
-  var parent = this.currentScope();
-  // Find the closest non-arrow function parent scope
-  while (parent.block.type === 'ArrowFunctionExpression') {
-    parent = parent.upper;
+  if (thisMaterialized(node)) {
+    var parent = this.currentScope();
+    // Find the closest non-arrow function parent scope
+    while (parent.block.type === 'ArrowFunctionExpression') {
+      parent = parent.upper;
+    }
+    // ensure its context is bound to the '_this' variable and
+    // save a reference to it in the node so all children of type
+    // 'ThisExpression' can replace themselves
+    node.arrowThis = this.ensureVar(this.arrowThis, parent, {
+      type: 'ThisExpression'
+    });
   }
-  // ensure its context is bound to the '_this' bound variable
-  // and save a reference to it
-  node.arrowThis = this.ensureVar(this.arrowThis, parent, {
-    type: 'ThisExpression'
-  });
 
   if (rv.body.type !== 'BlockStatement') {
     // expression, wrap into a body/return statement
--- test/fixtures/arrow_functions.js
+++ test/fixtures/arrow_functions.js
@@ -1,18 +1,19 @@
 var factorial = n => n > 1 ? n * factorial(n - 1) : 1;
 
-var fibonnaci = () => {
+var fibonnaci = (function() {
   var cache = [0, 1];
   return (n) => {
     if (n >= cache.length) cache[n] = fibonnaci(n - 1) + fibonnaci(n - 2);
     return cache[n];
   };
-};
-fibonnaci = fibonnaci();
+})();
 
 var obj = {
   method: function() {
     var arrow = () => {
-      return this;
+      var subArrow = () => this;
+      var notArrow = function() { return this; };
+      return [subArrow(), notArrow()];
     };
     return arrow();
   }
@@ -23,6 +24,10 @@ run({
   'Arrow functions': {
     'expression': () => expect(factorial(5)).to.eql(120),
     'block': () => expect(fibonnaci(20)).to.eql(6765),
-    'lexical context': () => expect(obj.method()).to.eq(obj)
+    'lexical context': () => {
+      var items = obj.method();
+      expect(items[0]).to.eq(obj);
+      expect(items[1]).to.not.eq(obj);
+    }
   }
 });
